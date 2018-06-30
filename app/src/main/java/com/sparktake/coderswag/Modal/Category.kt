a3f48417a85268f9a95d1b7e91c75deb4517c3f2
seanlau555 <victormte555@gmail.com>
2018-06-30 11:52:53 +0800

    lesson 01

---
+++ app/src/main/java/com/sparktake/coderswag/Modal/Category.kt
@@ -0,0 +1,7 @@
+package com.sparktake.coderswag.Modal
+
+/**
+ * Created by lauhon on 30/6/2018.
+ */
+class Category {
+}
---
+++ app/src/main/java/com/sparktake/coderswag/Services/DataService.kt
@@ -0,0 +1,7 @@
+package com.sparktake.coderswag.Services
+
+/**
+ * Created by lauhon on 30/6/2018.
+ */
+object DataService {
+}
