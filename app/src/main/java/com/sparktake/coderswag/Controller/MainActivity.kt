2379d68286f715928ba7ff23c1495b5202f5cf9a
seanlau555 <victormte555@gmail.com>
2018-07-01 12:20:32 +0800

    lesson 04

--- app/src/main/java/com/sparktake/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/sparktake/coderswag/Controller/MainActivity.kt
@@ -2,21 +2,19 @@ package com.sparktake.coderswag.Controller
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ArrayAdapter
-import com.sparktake.coderswag.Modal.Category
+import com.sparktake.coderswag.Adapters.CategoryAdapter
 import com.sparktake.coderswag.R
 import com.sparktake.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        adapter = ArrayAdapter(this,
-                android.R.layout.simple_expandable_list_item_1, DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
     }
 }
